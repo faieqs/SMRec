dbbf9ad790b12763ab54136a8d45aa363549a55e
faieqs <56736010+faieqs@users.noreply.github.com>
2019-10-18 19:09:26 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+This project contains the data and the code for the paper called "A Context-aware Recommendation-based System for Service Composition in Smart Environments"
+
+History.csv file contains the data which was generated based on a simulation of the case study presented in the paper called 'SMARTROAD' and is structured as follows:
+
+- Users_ID : Represents the ID of the user and respects the same numbering as the WSDREAM dataset.
+
+- Service_Model : Represents the name of the Service Model as depicted in the paper. The data contains 7 Service_Models (TrafficCongestion, RiskyVehicleSpeed, RiskyDriverHealth, RiskyDriverExperience, RiskyRoad, BadWeather, RiskyVehicleState). Only three of these service models were depicted in the paper.
+
+- Task_Name : Represents the name of the Task as depicted in the paper. The data contains 20 tasks (TJAW_I, ISA_E, RSS_E, IVS_I, ERP_I, ISA_I, NAAS_E, ERP_E, IVS_V, NAAS_I, ERS, RSS_V, AEVW, RWW, RSS_I, WWS, GSS_V, GSSS, GSS_I, TSS_V). Each task name is divided to two parts by the underscore character. The left part is the acronym of the task and the right part represents the level at which the task is executed by the service.
+
+- Service_ID : Represents the ID of the service and respects the same numbering as the WSDREAM dataset. Each service was associated with a task and hench has a Task_Name.
+
+- ResponseTime : Is a QoS measure that represents the lapse of time from the invocation request by the user to the service reply in seconds and is extracted from the WSDREAM dataset.
+
+- Throughput : Is a QoS measure that represents the amount of data transiting between the user and the service in Kbps and is extracted from the WSDREAM dataset.
---
+++ SMRec.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 10:27:18 2019
+
+@author: LIG
+"""
+
+import pandas as pd
+import pyfpgrowth as fpg
+
+def get_service_models(user_id, invocations):
+    userInvocations = invocations.groupby('Users_ID').get_group(user_id)
+    user_service_models = []
+    for index, user in userInvocations.iterrows():
+        sm = str(user.Service_Model)
+        if (sm not in user_service_models) and (sm != 'nan'):
+            user_service_models.append(user.Service_Model)
+    return user_service_models
+
+# read the data
+event_log = pd.read_csv('History.csv', sep=',')
+
+# declare threshold variable p
+p = 50
+
+# declare output variable
+r = []
+
+# create an atomic events filter
+is_atomic = event_log['Service_Model'].isna()
+
+#extract the atomic event
+atomic_events = event_log[is_atomic]
+
+# group the events by the task name
+grouped_atomic_events = atomic_events.groupby('Task_Name')
+
+# group the events by service model
+grouped_events_by_service_model = event_log.groupby('Service_Model')
+
+# get distinct service models
+distinct_service_models = event_log.Service_Model.unique().tolist()
+distinct_service_models = [x for x in distinct_service_models if str(x) != 'nan']
+
+# get the list of atomic events
+unique_atomic_events = atomic_events.Task_Name.unique().tolist()
+
+for uat in unique_atomic_events:
+    users_gae=[]
+    # get the list of users who invoked the atomic task uat
+    for index, gae in grouped_atomic_events.get_group(uat).iterrows():
+        users_gae.append(gae.Users_ID)
+
+    service_models = []
+    
+    # get the service models invoked by each user who also invoked the atomic event
+    for u in users_gae:
+        service_models.append(get_service_models(u, event_log))
+    
+    # for each service model, count all the users and the number of users who invoked the service model
+    # then check the patterns surpassing the threshold in the service models from the previous step
+    # and then add it to the list if it doesn't exist already
+    for distinct_service_model in distinct_service_models:
+        gae_service_models = grouped_events_by_service_model.get_group(distinct_service_model)
+        frequent_service_models = fpg.find_frequent_patterns(service_models, gae_service_models.Users_ID.nunique() * p/100)
+        if frequent_service_models:
+            most_frequent_sm = frequent_service_models.popitem()[0][0]
+            if most_frequent_sm and ([most_frequent_sm, uat] not in r) :
+                r.append([most_frequent_sm, uat])
+print('This is the result')
+print(r)
+                        
+        
